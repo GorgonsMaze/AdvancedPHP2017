154e0d7336ca5b5bbedeb3c140e57bf9ececafb2
ian <ian_arsenault@protonmail.com>
2017-04-26 00:57:49 -0400

    fixed session error

--- week3/Lab3/accounts/signup.php
+++ week3/Lab3/accounts/signup.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Add Address</title>
+        <title>Sign Up</title>
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.0/css/bulma.min.css">
         <link rel='stylesheet' type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
