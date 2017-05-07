c008cfb9c5b9ed25c94d9af30b13e9e73d73cb5c
ian <ian_arsenault@protonmail.com>
2017-05-07 15:32:31 -0400

    Comments on angular functionality

--- week6/lab/js/phone-detail.controller.js
+++ week6/lab/js/phone-detail.controller.js
@@ -1,11 +1,16 @@
+// Controller function for phone details
 (function () {
+    // Use strict javascript
     'use strict';
+    // Assigns phone detail controller to main module
     angular
         .module('app')
         .controller('PhoneDetailController', PhoneDetailController);
 
+    // Injects parameters to be used in controller
     PhoneDetailController.$inject = ['$routeParams', 'PhonesService'];
     
+    // Function to define the controller
     function PhoneDetailController($routeParams, PhonesService) {
         var vm = this;
         
--- week6/lab/js/phones.service.js
+++ week6/lab/js/phones.service.js
@@ -1,12 +1,18 @@
+/* Service file that allows us to make ajax calls usig
+ * built in features. Allows getting phones based on url, and find 
+ * phones based on the id
+ */
 (function() {
-    
+    // Use strict Javascript
     'use strict';
+    // Define the service for main module
     angular
         .module('app')
         .factory('PhonesService', PhonesService);
-
+    // Inject parameters to PhoneServiceSS
     PhonesService.$inject = ['$http', 'REQUEST'];
     
+    // Defines Phones Service
     function PhonesService($http, REQUEST) {
         var url = REQUEST.Phones;
         var service = {
@@ -16,25 +22,29 @@
         return service;
         
         /////////
-        
+        // Get Phones function
         function getPhones() {
+            // Promise for URL get
             return $http.get(url)
                 .then(getPhonesComplete, getPhonesFailed);
-        
+            // Promise completed
             function getPhonesComplete(response) {
                 return response.data;
             }
-            
+            // Promise error
             function getPhonesFailed(error) {
                 return [];
             }
         }
-        
+        // Find Phone by ID Function
         function findPhone(id) {
+            // Promise for get phone
             return getPhones()
                 .then(function(data) {
+                    // returns phone data
                     return findPhoneComplete(data);;
                 });
+            // Function that looks for specific phone
             function findPhoneComplete(data) {
                 var results = {};
                 
